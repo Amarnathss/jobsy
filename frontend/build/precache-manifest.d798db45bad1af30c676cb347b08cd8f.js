947a640889ccc9ee7cdc38181482bf159eb603b2
animesh624 <animesh624d@gmail.com>
2023-08-17 10:19:09 +0530

    merged frontend and backend

---
+++ frontend/build/precache-manifest.d798db45bad1af30c676cb347b08cd8f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6718bc7dc02aa02945b755cf1d451904",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70ff03f90342a6079b7f",
+    "url": "/static/css/main.f7229279.chunk.css"
+  },
+  {
+    "revision": "ec1545642d18b97f93a7",
+    "url": "/static/js/2.76a46f2f.chunk.js"
+  },
+  {
+    "revision": "306f3bf9d2f1ee8399ad560a7731d0d6",
+    "url": "/static/js/2.76a46f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ff03f90342a6079b7f",
+    "url": "/static/js/main.30a68ee5.chunk.js"
+  },
+  {
+    "revision": "c6984952d85af073ee5c",
+    "url": "/static/js/runtime-main.74878216.js"
+  },
+  {
+    "revision": "930fddd18770fc1bfa132ab3842032ea",
+    "url": "/static/media/banner.930fddd1.svg"
+  },
+  {
+    "revision": "66d2cc24ec80dcd7f6e8592bf25561cc",
+    "url": "/static/media/cook.66d2cc24.png"
+  },
+  {
+    "revision": "77f495189f25314670dd81db65574760",
+    "url": "/static/media/logo.77f49518.PNG"
+  },
+  {
+    "revision": "cdc2002673aa0dbdb3d3390c8ee3a9cc",
+    "url": "/static/media/peon.cdc20026.png"
+  },
+  {
+    "revision": "e1ef47de63257a0c52843b7cfc56564a",
+    "url": "/static/media/site.e1ef47de.png"
+  },
+  {
+    "revision": "88f614c1f4f2ba201665cc426b119996",
+    "url": "/static/media/view.88f614c1.png"
+  }
+]);
